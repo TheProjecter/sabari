9b142da8d16bdb02563e87a5dd67c89ca947db09
siranthony1247 <siranthony1247@2024a729-f91e-4dec-e509-56ec2c263716>
2010-11-08 20:31:21 +0000

    Move old versions to tags.

--- imagemanager.py
+++
@@ -1,7 +0,0 @@
-
-class Manager:
-    def __init__(self, form):
-        pass
-        
-    def work(self):
-        pass 
--- viewer-resizer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import libviewer
-from cgi import FieldStorage
-
-libviewer.putdata(FieldStorage())
