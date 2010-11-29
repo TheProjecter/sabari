94e058e2e6f4c64d5cc74a0c84f1438981b06013
siranthony1247 <siranthony1247@2024a729-f91e-4dec-e509-56ec2c263716>
2010-11-29 00:16:24 +0000

    Add images db.

--- trunk/imagemanager.py
+++ trunk/imagemanager.py
@@ -1,7 +1,21 @@
 
+import sqlite3
+
 class Manager:
-    def __init__(self, form):
-        pass
+    def __init__(self):
+        self.queries = []        
         
-    def work(self):
-        pass 
+    def addToBase(self, name, path, sha1):
+        self.queries.append('INSERT INTO `images` (name, path, sha1) VALUES ("'+name+'", "'+path+'", "'+sha1+'")')
+
+    def commit(self, db):
+        if len(self.queries):
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+            for qw in self.queries:
+                cursor.execute(qw)                
+            conn.commit()
+            cursor.close()
+ 
+
+            
